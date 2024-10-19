cf0f3c61760dd7ab3683d4f65f5557df9bfbc477
rvitanov <rvitanov@qualibooth.com>
2024-10-19 19:11:04 +0300

    add skhd module

--- home/modules/skhd.nix
+++ home/modules/skhd.nix
@@ -22,60 +22,62 @@ in
     };
   };
 
-  # Conditional configuration based on whether skhd is enabled
-  config = mkIf config.services.skhd.enable {
-    # Install skhd package
-    home.packages = [ pkgs.skhd ];
+  # Merged configuration for enabling and disabling the service
+  config = mkMerge [
+    (mkIf config.services.skhd.enable {
+      # Install skhd package
+      home.packages = [ pkgs.skhd ];
 
-    # Create skhdrc file with user's configuration
-    home.file = {
-      "${skhdConfigFile}".text = ''
-        # Default skhd config
-        # You can place custom configuration here
-        cmd - return : open -a Terminal
+      # Create skhdrc file with user's configuration
+      home.file = {
+        "${skhdConfigFile}".text = ''
+          # Default skhd config
+          # You can place custom configuration here
+          cmd - return : open -a Terminal
 
-        ${config.services.skhd.extraConfig}
+          ${config.services.skhd.extraConfig}
+        '';
+      };
+
+      # Create the launchd agent to start skhd on macOS
+      home.activation.installSkhdService = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        mkdir -p ~/Library/LaunchAgents
+        cat > ${launchAgentPath} <<EOF
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+          <dict>
+            <key>Label</key>
+            <string>com.user.skhd</string>
+            <key>ProgramArguments</key>
+            <array>
+              <string>${pkgs.skhd}/bin/skhd</string>
+              <string>-c</string>
+              <string>${skhdConfigFile}</string>
+            </array>
+            <key>RunAtLoad</key>
+            <true/>
+            <key>KeepAlive</key>
+            <true/>
+            <key>StandardErrorPath</key>
+            <string>/tmp/skhd.err</string>
+            <key>StandardOutPath</key>
+            <string>/tmp/skhd.out</string>
+          </dict>
+        </plist>
+        EOF
+        launchctl load ${launchAgentPath}
       '';
-    };
+    })
 
-    # Create the launchd agent to start skhd on macOS
-    home.activation.installSkhdService = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      mkdir -p ~/Library/LaunchAgents
-      cat > ${launchAgentPath} <<EOF
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>com.user.skhd</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>${pkgs.skhd}/bin/skhd</string>
-            <string>-c</string>
-            <string>${skhdConfigFile}</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>StandardErrorPath</key>
-          <string>/tmp/skhd.err</string>
-          <key>StandardOutPath</key>
-          <string>/tmp/skhd.out</string>
-        </dict>
-      </plist>
-      EOF
-      launchctl load ${launchAgentPath}
-    '';
-  } // End of "enable" block
-
-  // Cleanup logic when skhd is disabled
-  config = mkIf (!config.services.skhd.enable) {
-    home.activation.removeSkhdService = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if [ -f ${launchAgentPath} ]; then
-        launchctl unload ${launchAgentPath}
-        rm -f ${launchAgentPath}
-      fi
-    '';
-  };
+    # Cleanup logic when skhd is disabled
+    (mkIf (!config.services.skhd.enable) {
+      home.activation.removeSkhdService = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        if [ -f ${launchAgentPath} ]; then
+          launchctl unload ${launchAgentPath}
+          rm -f ${launchAgentPath}
+        fi
+      '';
+    })
+  ];
 }
