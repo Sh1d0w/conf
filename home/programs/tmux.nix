3df125da9328da345a573acc20a03dbc320a3a07
rvitanov <rvitanov@qualibooth.com>
2024-10-19 12:37:45 +0300

    wip

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -39,6 +39,7 @@
     ];
 
     extraConfig = ''
+      set -g prefix ^A
       bind -T copy-mode-vi 'v' send -X begin-selection
       bind -T copy-mode-vi 'y' send -X copy-pipe-and-cancel !
       bind -T copy-mode-vi 'z' send -X rectangle-toggle
